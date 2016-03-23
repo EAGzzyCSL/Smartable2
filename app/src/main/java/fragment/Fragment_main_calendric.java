ec9410251b4b5e4ce344dbbff18418109f1fc94e
EAGzzyCSL <eagzzycsl@github.com>
2016-03-24 00:18:17 +0800

    右露边drawer的实现，目前使用FrameLayout能够几乎实现，除了抽屉默认是打开无法调整，尝试使用覆写onMeasure来实现前先commit。

--- app/src/main/java/fragment/Fragment_main_calendric.java
+++ app/src/main/java/fragment/Fragment_main_calendric.java
@@ -4,9 +4,10 @@ package fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -32,16 +33,18 @@ public class Fragment_main_calendric extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        setHasOptionsMenu(true);
         View v = inflater.inflate(R.layout.fragment_main_calendric, container, false);
-        toolbar = (Toolbar) v.findViewById(R.id.toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
         return v;
     }
+
 
 }
--- app/src/main/java/fragment/Fragment_main_smart.java
+++ app/src/main/java/fragment/Fragment_main_smart.java
@@ -4,53 +4,33 @@ package fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
 
-import adapter.Adapter_main_kind_recyclerView;
-import adapter.Adapter_main_viewPager_kind;
 import bit.eagzzycsl.smartable2.R;
-import entry.Entry;
-import entry.EntryNote;
 
 public class Fragment_main_smart extends Fragment {
     private View myView;
-    private Toolbar toolbar;
     private FloatingActionButton fab_add;
-    private TabLayout tabLayout_kind;
-    private ViewPager viewPager_kind;
-    private Adapter_main_viewPager_kind adapter_main_viewPager_kind;
+    private Fragment_main_smart_classify fragment_main_smart_classify;
+    private Fragment_main_smart_serialize fragment_main_smart_serialize;
 
     public Fragment_main_smart() {
         // Required empty public constructor
     }
 
-
-    public Toolbar getToolbar() {
-        return toolbar;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
+
         myView = inflater.inflate(R.layout.fragment_main_smart, container, false);
         myFindView();
         myCreate();
@@ -59,49 +39,41 @@ public class Fragment_main_smart extends Fragment {
     }
 
     private void myFindView() {
-        toolbar = (Toolbar) myView.findViewById(R.id.toolbar);
         fab_add = (FloatingActionButton) myView.findViewById(R.id.fab);
-        tabLayout_kind = (TabLayout) myView.findViewById(R.id.tabLayout_kind);
-        viewPager_kind = (ViewPager) myView.findViewById(R.id.viewPager_kind);
+        fragment_main_smart_classify = (Fragment_main_smart_classify) getChildFragmentManager().findFragmentById(R.id.fragment_main_smart_classify);
+        fragment_main_smart_serialize = (Fragment_main_smart_serialize) getChildFragmentManager().findFragmentById(R.id.fragment_main_smart_serialize);
     }
 
     private void mySetView() {
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        viewPager_kind.setAdapter(adapter_main_viewPager_kind);
-        tabLayout_kind.setupWithViewPager(viewPager_kind);
-        tabLayout_kind.setTabsFromPagerAdapter(adapter_main_viewPager_kind);
+        if (true) {
+            getChildFragmentManager().beginTransaction().hide(fragment_main_smart_serialize).commit();
+        } else {
+            getChildFragmentManager().beginTransaction().hide(fragment_main_smart_classify).commit();
+        }
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("fab_add", "click");
+                fragment_main_smart_classify.closeDraw();
+            }
+        });
 
     }
 
     private void myCreate() {
-        final RecyclerView[] pages = new RecyclerView[]{
-                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
-                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
-                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
-                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
-        };
-        for (RecyclerView r : pages) {
-            r.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
-            r.setItemAnimator(new DefaultItemAnimator());
-        }
-        View[] views = new View[pages.length];
-        int[] sort = new int[]{3, 2, 0, 1};
-        for (int i = 0; i < views.length; i++) {
-            pages[i].setAdapter(new Adapter_main_kind_recyclerView(new ArrayList<Entry>() {
-                {
-                    this.add(new EntryNote("hello"));
-                }
-            }, getActivity()));
-            views[sort[i]] = pages[i];
-        }
-        adapter_main_viewPager_kind
-                = new Adapter_main_viewPager_kind(views,
-                getActivity().getResources().
-                        getStringArray(R.array.activity_main_smart_pager_title), sort);
-
-
     }
 
+    public int  switchView() {
+        /*0表示当前是分类，1表示当前是序列化，用于设置图标*/
+        if (fragment_main_smart_serialize.isHidden()) {
+
+            getChildFragmentManager().beginTransaction().hide(fragment_main_smart_classify).show(fragment_main_smart_serialize).commit();
+            return 1;
+        } else {
+            getChildFragmentManager().beginTransaction().hide(fragment_main_smart_serialize).show(fragment_main_smart_classify).commit();
+            return 0;
+        }
+    }
 
 
 }
