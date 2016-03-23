eb2766562181b64c39d774b157b30c4968086a62
EAGzzyCSL <eagzzycsl@outlook.com>
2016-03-24 00:18:17 +0800

    右露边drawer的实现，目前使用FrameLayout能够几乎实现，除了抽屉默认是打开无法调整，尝试使用覆写onMeasure来实现前先commit。

--- app/src/androidTest/java/bit/eagzzycsl/smartable2/ApplicationTest.java
+++ app/src/androidTest/java/bit/eagzzycsl/smartable2/ApplicationTest.java
@@ -1,7 +1,11 @@
 package bit.eagzzycsl.smartable2;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.test.ApplicationTestCase;
+import android.view.View;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
--- app/src/main/java/fragment/Fragment_main_smart_classify.java
+++ app/src/main/java/fragment/Fragment_main_smart_classify.java
@@ -1,15 +1,36 @@
 package fragment;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
+import adapter.Adapter_main_kind_recyclerView;
+import adapter.Adapter_main_viewPager_kind;
 import bit.eagzzycsl.smartable2.R;
+import entry.Entry;
+import entry.EntryNote;
+import view.EdgeDrawerLayout;
 
 public class Fragment_main_smart_classify extends Fragment {
-
+    private View myView;
+    private TabLayout tabLayout_kind;
+    private ViewPager viewPager_kind;
+    private LinearLayout linearLayout_main;
+    private EdgeDrawerLayout edgeDrawerLayout;
+    private Adapter_main_viewPager_kind adapter_main_viewPager_kind;
     public Fragment_main_smart_classify() {
         // Required empty public constructor
 
@@ -25,10 +46,64 @@ public class Fragment_main_smart_classify extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_smart_classify, container, false);
+        myView = inflater.inflate(R.layout.fragment_main_smart_classify, container, false);
+        myFindView();
+        myCreate();
+        mySetView();
+        return myView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+//        edgeDrawerLayout.closeDrawer();
+    }
+
+    private void myFindView() {
+        tabLayout_kind = (TabLayout) myView.findViewById(R.id.tabLayout_kind);
+        viewPager_kind = (ViewPager) myView.findViewById(R.id.viewPager_kind);
+        linearLayout_main=(LinearLayout)myView.findViewById(R.id.linearLayout_main);
+        edgeDrawerLayout=(EdgeDrawerLayout)myView.findViewById(R.id.edgeDrawerLayout);
+        linearLayout_main.setVisibility(View.INVISIBLE);
+
+    }
+    private void mySetView() {
+        viewPager_kind.setAdapter(adapter_main_viewPager_kind);
+        tabLayout_kind.setupWithViewPager(viewPager_kind);
+        tabLayout_kind.setTabsFromPagerAdapter(adapter_main_viewPager_kind);
+
+    }
+    private void myCreate() {
+        final RecyclerView[] pages = new RecyclerView[]{
+                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
+                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
+                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
+                (RecyclerView) View.inflate(getActivity(), R.layout.adapter_main_smart_pager, null),
+        };
+        for (RecyclerView r : pages) {
+            r.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
+            r.setItemAnimator(new DefaultItemAnimator());
+        }
+        View[] views = new View[pages.length];
+        int[] sort = new int[]{3, 2, 0, 1};
+        for (int i = 0; i < views.length; i++) {
+            pages[i].setAdapter(new Adapter_main_kind_recyclerView(new ArrayList<Entry>() {
+                {
+                    this.add(new EntryNote("hello"));
+                }
+            }, getActivity()));
+            views[sort[i]] = pages[i];
+        }
+        adapter_main_viewPager_kind
+                = new Adapter_main_viewPager_kind(views,
+                getActivity().getResources().
+                        getStringArray(R.array.activity_main_smart_pager_title), sort);
+
+
+    }
     // TODO: Rename method, update argument and hook method into UI event
 
-
+    public void closeDraw(){
+        edgeDrawerLayout.toggleDrawer();
+    }
 }
