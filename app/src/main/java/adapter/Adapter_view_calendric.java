91227c392b9989b8072f464a1044d462ca4ac7e4
EAGzzyCSL <eagzzycsl@github.com>
2016-04-29 23:01:11 +0800

    进一步解耦日历的viewpager，由于studio2.1的更新gradle更新。

--- app/src/main/java/adapter/Adapter_view_calendric.java
+++ app/src/main/java/adapter/Adapter_view_calendric.java
@@ -1,11 +1,8 @@
 package adapter;
 
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,40 +10,74 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 import entry.EntrySchedule;
-import my.MyDate;
-import my.MyTime;
-import test.ScheduleCaseProvider;
 import view.CalendricCombineDayView;
+import view.CalendricPagerView;
+import view.CalendricViewItemProvider;
+import view.EnumCalendricViewType;
 
 
 public class Adapter_view_calendric extends PagerAdapter {
-    private LinkedList<CalendricCombineDayView> pagers;
-    private Context context;
-    private MyDate date;
+    private EnumCalendricViewType enumViewType;
 
-    public LinkedList<CalendricCombineDayView> getMySimpleByDayViews() {
-        return this.pagers;
+    public EnumCalendricViewType getEnumViewType() {
+        return this.enumViewType;
     }
 
-    public Adapter_view_calendric(Context context, MyDate date) {
+    private LinkedList<View> pagers;
+    private Context context;
+    private CalendricPagerView page_a;
+    private CalendricPagerView page_b;
+    private CalendricPagerView page_c;
+
+    public CalendricPagerView getPage_a() {
+        return this.page_a;
+    }
+
+    public CalendricPagerView getPage_b() {
+        return this.page_b;
+    }
+
+    public CalendricPagerView getPage_c() {
+        return this.page_c;
+    }
+
+    private CalendricViewItemProvider itemProvider;
+
+    public ArrayList<EntrySchedule> getScheduleFromItemProvider() {
+        return itemProvider.getItems(0);
+    }
+
+
+    public Adapter_view_calendric(Context context, EnumCalendricViewType enumViewType, CalendricViewItemProvider itemProvider) {
         this.context = context;
-        this.date = date;
-        this.pagers = new LinkedList<CalendricCombineDayView>();
+        this.pagers = new LinkedList<>();
+        this.itemProvider = itemProvider;
+        this.enumViewType = enumViewType;
+        switch (enumViewType) {
+            case Day: {
+                CalendricCombineDayView a = new CalendricCombineDayView(context, null);
+                pagers.add(a);
+                CalendricCombineDayView b = new CalendricCombineDayView(context, null);
+                pagers.add(b);
+                CalendricCombineDayView c = new CalendricCombineDayView(context, null);
+                pagers.add(c);
+                this.page_a = a;
+                this.page_b = b;
+                this.page_c = c;
+                break;
+            }
+            case ThreeDay: {
+                break;
+            }
+            case Week: {
+                break;
+            }
+            case Month: {
+                break;
+            }
+        }
         //TODO 布局参数的问题
-        CalendricCombineDayView a = new CalendricCombineDayView(context, null);
-        a.setEntrySchedule(ScheduleCaseProvider.getSchedule(14));
-        a.setTextView_showDay("14");
-        pagers.add(a);
-        //
-        CalendricCombineDayView b = new CalendricCombineDayView(context, null);
-        b.setEntrySchedule(ScheduleCaseProvider.getSchedule(15));
-        b.setTextView_showDay("15");
-        pagers.add(b);
-        //
-        CalendricCombineDayView c = new CalendricCombineDayView(context, null);
-        c.setEntrySchedule(ScheduleCaseProvider.getSchedule(16));
-        c.setTextView_showDay("16");
-        pagers.add(c);
+
     }
 
     @Override
@@ -62,14 +93,14 @@ public class Adapter_view_calendric extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
 
-        CalendricCombineDayView s = pagers.get(position);
+        View s = pagers.get(position);
         container.addView(s, 0);
         return s;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        CalendricCombineDayView s = pagers.get(position);
+        View s = pagers.get(position);
         container.removeView(s);
     }
 
